c7a6688c6139beafc34788942da96c3992a2be0f
Joe Tsai <giltwg@gmail.com>
2016-02-05 16:18:30 +0800

    joe's test

--- Team03WebProj/src/eeit/Servlet01.java
+++ Team03WebProj/src/eeit/Servlet01.java
@@ -12,17 +12,11 @@ import javax.servlet.http.HttpServletResponse;
 public class Servlet01 extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("Servlet01");
+		
+		System.out.println("hahaha");
 		
 		String location="/Team03WebProj/index.jsp";
-		
-
 		resp.sendRedirect(location);
-		System.out.println("13212123123");
-
-		System.out.println("hahahaha");
-		
-
 	}
 
 	@Override
